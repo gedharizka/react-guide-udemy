29d291b490cd3bc0326261e99564115ba1b6ddbd
gedha rizka <gedharizka@gmail.com>
2020-01-13 16:22:53 +0700

    Re using component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React,{Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render(){
@@ -8,6 +9,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi i'm now learn react APp Understanding component</h1>
         <p>Ini bekerja</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null,'Ini juga berhasil jalan ') );
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+const person =()=>{
+  return <p>I'm Person !</p>
+};
+
+export default person;
