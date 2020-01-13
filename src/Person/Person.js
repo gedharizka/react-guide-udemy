b183e7fb6c3bfbf43a13157130e28aad27546dd9
gedha rizka <gedharizka@gmail.com>
2020-01-13 16:35:45 +0700

    Dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const person =()=>{
-  return <p>I'm Person !</p>
+  return <p>I'm Person I am {Math.floor(Math.random()*30)} years old !</p>
 };
 
 export default person;
