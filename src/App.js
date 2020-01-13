14a2d16eb93185ee65f5f3603ea5d5c81a0d166a
gedha rizka <gedharizka@gmail.com>
2020-01-13 14:52:18 +0700

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi i'm now learn react APp</h1>
-    </div>
-  );
-}
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi i'm now learn react APp Understanding component</h1>
+      </div>
+    );
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null,'Ini juga berhasil jalan ') );
+    }
+  }
 
 export default App;
