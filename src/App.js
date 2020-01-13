e12c2b45ba9775a2d12e322aa77be9ffd8be765d
gedha rizka <gedharizka@gmail.com>
2020-01-13 18:51:06 +0700

    Handling Event

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state ={
+    persons :[
+      {name :"max", age: 11},
+      {name: "Wellber", age:20},
+      {name: "Swazer", age:39}
+    ]
+  }
+
+  switchNameHandler =()=>{
+    console.log('clicked!');
+    
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Hi i'm now learn react APp Understanding component</h1>
         <p>Ini bekerja</p>
-        <Person name="max aung" age="25"></Person>
-        <Person name="well" age="18">My Hobbies : Football</Person>
-        <Person name="beer" age="35"></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies : Football</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
 
       </div>
     );
