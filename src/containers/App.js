5f3a2bac088179e8f8b99e4f617e5347e5f66c94
gedha rizka <gedharizka@gmail.com>
2020-02-17 15:25:19 +0700

    Better Project Structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@
 import React,{Component} from 'react';
 import './App.css';
 // import style from 'styled-components';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 import styled from 'styled-components';
 // import Radium, {StyleRoot} from 'radium';
 // import classes from '*.module.css';
